7afee26df92431730c9e711d766c0211c0245d25
Tyler <tylerwesleyquinn@gmail.com>
2020-01-31 12:50:23 -0500

    imported dll/binarysearchtree from earlier in the week to implement names solution / mvp complete

--- names/names.py
+++ names/names.py
@@ -1,4 +1,8 @@
 import time
+import sys
+sys.path.append('./doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList, BinarySearchTree
+
 
 start_time = time.time()
 
@@ -11,10 +15,15 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+bst = BinarySearchTree(names_1[0])
+
+for name_1 in names_1[1:]:
+    bst.insert(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
