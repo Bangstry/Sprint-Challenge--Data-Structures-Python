4c9c5d5c814bb5c385d1ffb76c9c4dbde1d79e43
Tyler <tylerwesleyquinn@gmail.com>
2020-01-31 11:44:41 -0500

    buffer without stretch, may come back afterwards

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,25 +8,38 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail 
+        else:
+            if self.current.next is not None:
+                self.current.next.value = item
+                self.current = self.current.next
+            else:
+                self.storage.head.value = item
+                self.current = self.storage.head
 
     def get(self):
-        # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        cur_node = self.storage.head
+        while cur_node is not None:
+            if cur_node.value is not None:
+                list_buffer_contents.append(cur_node.value)
+            cur_node = cur_node.next
 
         return list_buffer_contents
 
-# ----------------Stretch Goal-------------------
+# # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
